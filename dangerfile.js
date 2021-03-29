dd6fef3c4f9bce9709952c55fcc1b14a10b47da8
dereck <djquock@gmail.com>
2021-03-28 23:30:26 -0700

    update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -4,7 +4,7 @@ const modifiedMD = danger.git.modified_files.join('\n');
 message(`Changed Files in this PR: \n - ${modifiedMD}`);
 
 // Warns if there are changes to package.json, and tags the team.
-const packageChanged = includes(danger.git.modified_files, 'package.json');
+const packageChanged = danger.git.modified_files.includes('package.json');
 if (packageChanged) {
   const title = ':lock: package.json';
   const idea = 'Changes were made to `package.json.`';
