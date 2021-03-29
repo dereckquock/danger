f0597f1e9fa9fe3d64534a287a33e1fea96b979d
dereck <djquock@gmail.com>
2021-03-28 23:43:22 -0700

    update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,12 +1,21 @@
-import { message, danger } from 'danger';
+import { danger, message, warn } from 'danger';
 
-const modifiedMD = danger.git.modified_files.join('\n');
-message(`Changed Files in this PR: \n - ${modifiedMD}`);
-
-// Warns if there are changes to package.json, and tags the team.
+// Warns if there are changes to package.json
 const packageChanged = danger.git.modified_files.includes('package.json');
 if (packageChanged) {
   const title = ':lock: package.json';
   const idea = 'Changes were made to `package.json.`';
   warn(`${title} - <i>${idea}</i>`);
+}
+
+const bigPRThreshold = 0;
+if (danger.github.pr.additions > bigPRThreshold) {
+  warn(':exclamation: Massive PR (' + ++errorCount + ')');
+  markdown(
+    `> (${errorCount}) : Pull Request size seems relatively large. Is it possible to separate this into smaller PRs to help with faster, easier review?`
+  );
+}
+
+if (danger.github.pr.deletions > danger.github.pr.additions) {
+  message(':100: Nice! You deleted more code than you added :100:');
 }
